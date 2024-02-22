d9d3c41dff104aa222fbe5a5662e01c1a8eefbfb
AthallaITS <5027231018@student.its.ac.id>
2024-02-22 20:15:26 +0700

    refactor dotenv import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import 'dotenv/config';
+import 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
